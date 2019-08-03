dcd3e195853ec05f765e792431b51b9a98902e51
Renato Lima <natenho@gmail.com>
2019-08-03 14:07:22 -0300

    Include request id in logs, Closes #34

--- src/Mockaco/Program.cs
+++ src/Mockaco/Program.cs
@@ -28,7 +28,9 @@ namespace Mockaco
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((_, configuration) => configuration.AddJsonFile(@"Settings/appsettings.json", optional: true, reloadOnChange: true))
             .UseSerilog((context, serilog) =>
-                serilog.WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+                serilog.WriteTo.Console(
+                    theme: AnsiConsoleTheme.Literate, 
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {RequestId} {Message:lj}{NewLine}{Exception}")
                 .ReadFrom.Configuration(context.Configuration))
             .UseStartup<Startup>();
     }
