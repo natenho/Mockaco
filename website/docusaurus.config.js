5b1cdee2e332efb17a8ef802904d7ed157d5c73e
Renato Lima <natenho@gmail.com>
2023-06-09 18:40:21 -0300

    Fix production website URL

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://natenho.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Mockaco',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
