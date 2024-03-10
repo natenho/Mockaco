7d9e32b8fc82890b509b01614c5d5d76ee39b6af
Renato Lima <natenho@gmail.com>
2024-03-10 18:40:35 -0300

    Simplify regex script test

--- test/Mockaco.AspNetCore.Tests/Templating/Scripting/ScriptRunnerFactoryTest.cs
+++ test/Mockaco.AspNetCore.Tests/Templating/Scripting/ScriptRunnerFactoryTest.cs
@@ -1,12 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace Mockaco.Tests.Templating.Scripting
 {
@@ -16,7 +9,7 @@ namespace Mockaco.Tests.Templating.Scripting
         [InlineData(@"JsonConvert.SerializeObject(new DateTime(2012, 04, 23, 18, 25, 43, 511, DateTimeKind.Utc))", @"\""2012-04-23T18:25:43\.511Z\""")]
         [InlineData(@"new PhoneNumbers().BrazilianPhoneNumber()", @"[0-9]+")]
         [InlineData(@"new Faker().Random.Guid().ToString()", @"[a-z0-9\-]+")]
-        [InlineData(@"new Regex(@"".*"").IsMatch(""abc"").ToString()", @"True")]
+        [InlineData(@"Regex.IsMatch(""abc"", "".*"").ToString()", @"True")]
         [InlineData(@"new[] {1, 2, 3, 4, 5, 6, 7}.Count().ToString()", @"7")]
         public async Task Can_Run_Scripts_From_Builtin_Namespaces(string input, string regexPattern)
         {
@@ -24,7 +17,6 @@ namespace Mockaco.Tests.Templating.Scripting
             var mockOptions = Moq.Mock.Of<IOptionsMonitor<MockacoOptions>>(o => o.CurrentValue == new MockacoOptions());
 
             var runner = new ScriptRunnerFactory(mockLogger, mockOptions);
-
             var result = await runner.Invoke<string, string>("", input);
 
             Assert.Matches(regexPattern, result);
